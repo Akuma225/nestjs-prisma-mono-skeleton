f1315a5ffd17290adb75608a9356bc5996106020
Akuma225 <cedricdatcha@gmail.com>
2024-07-17 11:22:47 +0000

    feat: Add bearer authentication to Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -59,6 +59,7 @@ async function bootstrap() {
     .setTitle(APP_NAME)
     .setDescription(BACK_OFFICE_DESCRIPTION)
     .setVersion(APP_VERSION)
+    .addBearerAuth()
     .build();
   const backOfficeDocument = SwaggerModule.createDocument(
     app,
